634bab246626950a46c16f0fb04bcefa96ce3bbb
Nico <nicomoranzoni@hotmail.com>
2024-09-13 19:59:56 -0300

    Ultimas modificiaciones, correcciones

--- Calculadora/funciones.py
+++ Calculadora/funciones.py
@@ -1,4 +1,4 @@
-def sumar (numero, numero_dos):
+def sumar (numero:int, numero_dos:int) -> int:
     '''
     - Funcion que se encarga de realizar suma
     - Recibe dos numeros
@@ -7,7 +7,7 @@ def sumar (numero, numero_dos):
     resultado = numero + numero_dos
     return resultado
 
-def resta (numero, numero_dos):
+def resta (numero:int, numero_dos:int) -> int:
 
     '''
     - Funcion que se encarga de realizar resta
@@ -18,18 +18,26 @@ def resta (numero, numero_dos):
     resultado = numero - numero_dos
     return resultado
 
-def dividir (numero, numero_dos):
+def dividir (numero:int, numero_dos:int) -> float:
 
     '''
     - Funcion que se encarga de realizar una division.
     - Recibe dos numeros.
     - Retorna el resultado de la division entre los parametros recibidos.
     '''
+    if numero_dos != 0:
+        resultado = numero / numero_dos
+    else:
+        #3 Opciones:
+        # resultado = 0
+        # resultado = None 
+        # resultado -> str con mensaje de error
+        resultado = "No se puede dividir por 0"
 
-    resultado = numero / numero_dos
     return resultado
 
-def multiplicar (numero, numero_dos):
+
+def multiplicar (numero:int, numero_dos:int) -> int:
     
     '''
     - Funcion que se encarga de realizar multiplicación
@@ -37,10 +45,11 @@ def multiplicar (numero, numero_dos):
     - Retorna el resultado de la multiplicación de los parametros recibidos.
     '''
 
+    
     resultado = numero * numero_dos
     return resultado 
 
-def calcular_potencia (numero, numero_dos):
+def calcular_potencia (numero:int, numero_dos:int) -> int:
 
     '''
     - Funcion que se encarga de realizar la potencia entre dos numeros
@@ -52,26 +61,20 @@ def calcular_potencia (numero, numero_dos):
     return resultado
 
 
-def calcular_resto (numero, numero_dos):
+def calcular_resto (numero:int, numero_dos:int) -> int:
 
     '''
     - Funcion que se encarga de calcular el resto
     - Recibe dos numeros
     - Retorna el resto de los operacion entre los parametros recibidos.
     '''
-    if numero_dos == 0:
-        return print("No es posible dividir por 0")
-    else: 
-        resultado = numero % numero_dos
-        return resultado
-
-def calcular_factorial (numero, numero_dos):
     
-    '''
-    - Funcion que se encarga de calcular el resto
-    - Recibe dos numeros
-    - Retorna el resto de los operacion entre los parametros recibidos.
-    '''       
+    if numero_dos != 0:
+        resultado = numero % numero_dos
+    else:
+        resultado = "No se puede dividir por cero, por lo que no se calculó el resto"
+    
+    return resultado
     
 def calcular_factorial (numero:int) -> int:
     '''
--- Calculadora/menu.py
+++ Calculadora/menu.py
@@ -2,6 +2,10 @@ import os
 from funciones import *
 
 def menu():
+    bandera_numero_uno = False
+    bandera_numero_dos = False
+    bandera_operaciones = False
+
     while(True):
         print("MENU CALCULADORA\n1.Ingresar Primer Operando\n2.Ingresar Segundo Operando\n3.Calcular Todas las operaciones\n4.Informar Resultados\n5.Salir")
         opcion = int(input("Su opcion: "))
@@ -9,33 +13,42 @@ def menu():
         if opcion == 1:
             
             primer_operando = int(input("Ingrese primer operando: "))
+            bandera_numero_uno = True
         elif opcion == 2:
             
             segundo_operando = int(input("Ingrese segundo operando: "))
+            bandera_numero_dos = True
         elif opcion == 3:
-            print("Calculo todas las operaciones")
-            sumar(primer_operando, segundo_operando)
-            resta(primer_operando, segundo_operando)
-            dividir(primer_operando, segundo_operando)
-            multiplicar(primer_operando, segundo_operando)
-            calcular_potencia(primer_operando, segundo_operando)
-            calcular_resto(primer_operando, segundo_operando)
-            calcular_factorial(primer_operando)
-            calcular_factorial(segundo_operando)
+            #Puedo ingresar a esta opcion
+            if bandera_numero_uno == True and bandera_numero_dos == True:
+                resultado_suma = sumar(primer_operando, segundo_operando)
+                resultado_resta = resta(primer_operando, segundo_operando)
+                resultado_divir = dividir(primer_operando, segundo_operando)
+                resultado_multiplicar = multiplicar(primer_operando, segundo_operando)
+                resultado_potencia = calcular_potencia(primer_operando, segundo_operando)
+                resultado_resto = calcular_resto(primer_operando, segundo_operando)
+                resultado_factorial_a = calcular_factorial(primer_operando)
+                resultado_factorial_b = calcular_factorial(segundo_operando)
+                print("Se calcularon todas las operaciones")
+                bandera_operaciones = True
+            else:
+                print("No se puede ingresar a la opcion 3 hasta no tener ambos operandos")
+
+
+            
         elif opcion == 4:
-            print("Informo todos los resultados")
-            print(f"""El resultado de sumar {primer_operando} + {segundo_operando} es: {sumar(primer_operando, segundo_operando)}
-            El resultado de restar {primer_operando} - {segundo_operando} es: {resta(primer_operando, segundo_operando)}
-            El resultado de dividir {primer_operando}/{segundo_operando} es: {dividir(primer_operando, segundo_operando)}
-            El resultado de multiplacar {primer_operando}*{segundo_operando}: {multiplicar(primer_operando, segundo_operando)}
-            {primer_operando} elevevado a la {segundo_operando} da como resultado: {calcular_potencia(primer_operando, segundo_operando)}
-            El resultado de {primer_operando} % {segundo_operando} es {calcular_resto(primer_operando, segundo_operando)}
-            El factorial de {primer_operando} es: {calcular_factorial(primer_operando)} y el factorial de {segundo_operando} es: {calcular_factorial(segundo_operando)}
-
-
-
-
-            """)
+            if bandera_operaciones == True:
+                print("Informo todos los resultados")
+                print(f"""El resultado de sumar {primer_operando} + {segundo_operando} es: {resultado_suma}
+                El resultado de restar {primer_operando} - {segundo_operando} es: {resultado_resta}
+                El resultado de dividir {primer_operando}/{segundo_operando} es: {resultado_divir}
+                El resultado de multiplacar {primer_operando}*{segundo_operando}: {resultado_multiplicar}
+                {primer_operando} elevevado a la {segundo_operando} da como resultado: {resultado_potencia}
+                El resultado de {primer_operando} % {segundo_operando} es {resultado_resto}
+                El factorial de {primer_operando} es: {resultado_factorial_a} y el factorial de {segundo_operando} es: {resultado_factorial_b}  
+                        """)
+            else:
+                print("No puede ingresar a ver las operaciones")
             print()
         elif opcion == 5:
             print("Saliendo...")
